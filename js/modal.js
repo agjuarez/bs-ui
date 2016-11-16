383413a3651dddd7fb8008cf9dedf1f1dd57c255
agjuarez <agjuarez@up.edu.ph>
2016-11-17 04:08:30 +0800

    Add lots
    Add favicon
    Finally fixed masonry
    Add photos to photos tab
    Add photos to works tab
    Add poem to poems tab

---
+++ js/modal.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+$('.thumbnail').click(function(){
+      $('.modal-body').empty();
+  	$($(this).parents('div').html()).appendTo('.modal-body');
+  	$('#myModal').modal({show:true});
+});
+});
