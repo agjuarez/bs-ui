d77a05b55ca742ebb94a0b84eba9aed8e7ea8b2c
agjuarez <agjuarez@up.edu.ph>
2016-11-17 04:10:14 +0800

    Revert "Add lots"
    This reverts commit 383413a3651dddd7fb8008cf9dedf1f1dd57c255.

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -9,6 +9,7 @@ $(window).scroll(function() {
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
+         $(".navbar").hide().fadeOut("slow");
 
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
